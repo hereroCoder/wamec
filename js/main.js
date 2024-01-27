32c34b432607eba692b84de44b5a66a92e547003
Wame Chibamo <wamechibamo76@gmail.com>
2024-01-27 04:31:17 +0000

    added projects section

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//menu functions
+function openMenu(){
+  document.querySelector('nav').style.right = '0';
+  document.querySelector('nav').style.boxShadow= "0 0 0 100vw rgba(0,0,0,0.8)";
+ }
+ 
+ function closeMenu(){
+  document.querySelector('nav').style.right = '-200%';
+ };
