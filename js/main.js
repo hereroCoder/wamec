7dd2c1cefbf2c1c79be93d2ff2ae56dcb6c1e893
Wame Chibamo <wamechibamo76@gmail.com>
2024-01-27 16:44:54 +0000

    added certificates section

--- js/main.js
+++ js/main.js
@@ -7,3 +7,30 @@ function openMenu(){
  function closeMenu(){
   document.querySelector('nav').style.right = '-200%';
  };
+
+ //certificates show more
+
+ loadBtn = document.querySelector('.load-btn .btn');
+
+if(loadBtn){
+  loadBtn.onclick = ()=>{
+  document.querySelectorAll('.certificates .certificates-container .hide').forEach(
+    show=>{
+      if(show.style.display=='block'){
+           show.style.display='none';
+            loadBtn.innerHTML='show more';
+          
+      }
+      else{
+        show.style.display='block';
+         loadBtn.innerHTML='show less';
+       
+
+      }
+   
+    }
+  )
+  
+};
+
+}
