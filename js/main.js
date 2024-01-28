4e5897b3c12ea93de32a777db41badd96f5307e5
Wame Chibamo <wamechibamo76@gmail.com>
2024-01-28 15:28:04 +0000

    added for close menu on navlink clicked

--- js/main.js
+++ js/main.js
@@ -15,12 +15,19 @@ document.addEventListener('scroll', () => {
 //menu functions
 function openMenu(){
   document.querySelector('nav').style.right = '0';
-  document.querySelector('nav').style.boxShadow= "0 0 0 100vw rgba(0,0,0,0.8)";
  }
  
  function closeMenu(){
   document.querySelector('nav').style.right = '-200%';
  };
+
+
+//close menu on item click
+document.querySelectorAll('.navlink').forEach(link=>{
+  link.addEventListener('click', () => {
+    closeMenu();
+  })
+})
 
 
 
